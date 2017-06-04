e0a64d4ad56d05e5001800fd665ccc561e54d86d
bryonyoni <bryonyoni@gmail.com>
2017-06-04 23:51:21 +0300

    Added classes to corresponding subdirectories
    added an AdSlideFragment, CreateAccountActivity, LoginActivity and
    MainActivity class, Advert and User class, and AdSlidePagerAdapter to the
    corresponding ui, models and adapters subdirectories.

---
+++ app/src/main/java/com/bry/adstudio/adapters/AdSlidePagerAdapter.java
@@ -0,0 +1,8 @@
+package com.bry.adstudio.adapters;
+
+/**
+ * Created by bryon on 6/4/2017.
+ */
+
+public class AdSlidePagerAdapter {
+}
---
+++ app/src/main/java/com/bry/adstudio/ui/CreateAccountActvity.java
@@ -0,0 +1,15 @@
+package com.bry.adstudio.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bry.adstudio.R;
+
+public class CreateAccountActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_actvity);
+    }
+}
