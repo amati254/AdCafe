e859c1181db4230291cf169000ca6d30a3ef1504
bryonyoni <bryonyoni@gmail.com>
2017-08-29 11:54:08 +0300

    Added a countdown timer

--- app/src/main/java/com/bry/adstudio/adapters/AdCounterBar.java
+++ app/src/main/java/com/bry/adstudio/adapters/AdCounterBar.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.CountDownTimer;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.animation.RotateAnimation;
@@ -27,11 +28,13 @@ import com.mindorks.placeholderview.annotations.View;
 
 @Layout(R.layout.top_bar_view)
 public class AdCounterBar {
-    @View(R.id.adCounter)
-    private TextView adCounter;
+    @View(R.id.adCounter) private TextView adCounter;
+    @View(R.id.progressBarTimer) private ProgressBar progressBarTimer;
+    @View(R.id.textViewTime) private TextView textViewTime;
 
     private Context mContext;
     private PlaceHolderView mPlaceHolderView;
+    private CountDownTimer mCountDownTimer;
 
     public AdCounterBar(Context context, PlaceHolderView PlaceHolderView){
         mContext = context;
@@ -55,8 +58,26 @@ public class AdCounterBar {
            String message = "Broadcast has been received.";
             String adTotal = intent.getStringExtra(Constants.AD_TOTAL);
             adCounter.setText(adTotal);
+            startTimer();
             Log.d("AD_COUNTER_BAR - ",message);
 
         }
     };
+
+    private void startTimer(){
+        mCountDownTimer = new CountDownTimer(7*1000,100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long timeLeftInSeconds = millisUntilFinished/1000;
+                progressBarTimer.setProgress((int)timeLeftInSeconds*7);
+                textViewTime.setText(Integer.toString((int)timeLeftInSeconds));
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("Timer --- ","Timer has finnished");
+                progressBarTimer.setProgress(7*1000);
+            }
+        }.start();
+    }
 }
